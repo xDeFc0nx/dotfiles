cc192984a110697bedde0223040f29dffa77ddf1
Nehar Tale <nehartale8@gmail.com>
2026-06-18 20:55:47 +0200

    feat: Add support for Alacritty, Ghostty, and Starship terminals

--- dots/.config/quickshell/ii/scripts/colors/applycolor.sh
+++ dots/.config/quickshell/ii/scripts/colors/applycolor.sh
@@ -21,8 +21,8 @@ colorstrings=''
 colorlist=()
 colorvalues=()
 
-colornames=$(cat $STATE_DIR/user/generated/material_colors.scss | cut -d: -f1)
-colorstrings=$(cat $STATE_DIR/user/generated/material_colors.scss | cut -d: -f2 | cut -d ' ' -f2 | cut -d ";" -f1)
+colornames=$(grep -E '^[[:space:]]*\$' "$STATE_DIR/user/generated/material_colors.scss" | cut -d: -f1 | tr -d '\r\t ')
+colorstrings=$(grep -E '^[[:space:]]*\$' "$STATE_DIR/user/generated/material_colors.scss" | cut -d: -f2 | tr -d '\r\t ;' | cut -d '/' -f1)
 IFS=$'\n'
 colorlist=($colornames)     # Array of color names
 colorvalues=($colorstrings) # Array of color values
@@ -38,7 +38,11 @@ apply_kitty() {
   cp "$SCRIPT_DIR/terminal/kitty-theme.conf" "$STATE_DIR"/user/generated/terminal/kitty-theme.conf
   # Apply colors
   for i in "${!colorlist[@]}"; do
+    local clean_name="${colorlist[$i]#\$}"
+    local camel_name=$(echo "$clean_name" | sed -E 's/_([a-z])/\U\1/g')
     sed -i "s/${colorlist[$i]} #/${colorvalues[$i]#\#}/g" "$STATE_DIR"/user/generated/terminal/kitty-theme.conf
+    sed -i "s/#${clean_name}#/${colorvalues[$i]}/g" "$STATE_DIR"/user/generated/terminal/kitty-theme.conf
+    sed -i "s/#${camel_name}#/${colorvalues[$i]}/g" "$STATE_DIR"/user/generated/terminal/kitty-theme.conf
   done
 
   # Reload
@@ -46,6 +50,62 @@ apply_kitty() {
     return
   fi
   kill -SIGUSR1 $(pidof kitty)
+}
+
+apply_alacritty() {
+  if [ ! -f "$SCRIPT_DIR/terminal/alacritty-theme.toml" ]; then
+    echo "Template file not found for Alacritty theme. Skipping that."
+    return
+  fi
+  mkdir -p "$STATE_DIR"/user/generated/terminal
+  cp "$SCRIPT_DIR/terminal/alacritty-theme.toml" "$STATE_DIR"/user/generated/terminal/alacritty-theme.toml
+  
+  for i in "${!colorlist[@]}"; do
+    local clean_name="${colorlist[$i]#\$}"
+    local camel_name=$(echo "$clean_name" | sed -E 's/_([a-z])/\U\1/g')
+    sed -i "s/${colorlist[$i]} #/${colorvalues[$i]#\#}/g" "$STATE_DIR"/user/generated/terminal/alacritty-theme.toml
+    sed -i "s/#${clean_name}#/${colorvalues[$i]}/g" "$STATE_DIR"/user/generated/terminal/alacritty-theme.toml
+    sed -i "s/#${camel_name}#/${colorvalues[$i]}/g" "$STATE_DIR"/user/generated/terminal/alacritty-theme.toml
+  done
+
+  mkdir -p "$XDG_CONFIG_HOME/alacritty"
+  cp "$STATE_DIR/user/generated/terminal/alacritty-theme.toml" "$XDG_CONFIG_HOME/alacritty/alacritty.toml"
+}
+
+apply_ghostty() {
+  if [ ! -f "$SCRIPT_DIR/terminal/ghostty-theme.conf" ]; then
+    echo "Template file not found for Ghostty theme. Skipping that."
+    return
+  fi
+  mkdir -p "$STATE_DIR"/user/generated/terminal
+  cp "$SCRIPT_DIR/terminal/ghostty-theme.conf" "$STATE_DIR"/user/generated/terminal/ghostty-theme.conf
+  for i in "${!colorlist[@]}"; do
+    local clean_name="${colorlist[$i]#\$}"
+    local camel_name=$(echo "$clean_name" | sed -E 's/_([a-z])/\U\1/g')
+    sed -i "s/${colorlist[$i]} #/${colorvalues[$i]#\#}/g" "$STATE_DIR"/user/generated/terminal/ghostty-theme.conf
+    sed -i "s/#${clean_name}#/${colorvalues[$i]}/g" "$STATE_DIR"/user/generated/terminal/ghostty-theme.conf
+    sed -i "s/#${camel_name}#/${colorvalues[$i]}/g" "$STATE_DIR"/user/generated/terminal/ghostty-theme.conf
+  done
+}
+
+apply_starship() {
+  if [ ! -f "$SCRIPT_DIR/terminal/starship.toml" ]; then
+    echo "Template file not found for Starship. Skipping that."
+    return
+  fi
+  mkdir -p "$STATE_DIR"/user/generated/terminal
+  cp "$SCRIPT_DIR/terminal/starship.toml" "$STATE_DIR"/user/generated/terminal/starship.toml
+  for i in "${!colorlist[@]}"; do
+    local clean_name="${colorlist[$i]#\$}"
+    local camel_name=$(echo "$clean_name" | sed -E 's/_([a-z])/\U\1/g')
+    sed -i "s/${colorlist[$i]} #/${colorvalues[$i]#\#}/g" "$STATE_DIR"/user/generated/terminal/starship.toml
+    sed -i "s/#${clean_name}#/${colorvalues[$i]}/g" "$STATE_DIR"/user/generated/terminal/starship.toml
+    sed -i "s/#${camel_name}#/${colorvalues[$i]}/g" "$STATE_DIR"/user/generated/terminal/starship.toml
+  done
+
+  mkdir -p "$XDG_CONFIG_HOME"
+  cp "$STATE_DIR"/user/generated/terminal/starship.toml "$XDG_CONFIG_HOME/starship.toml"
+  kill -WINCH $(pgrep -f fish) 2>/dev/null || true
 }
 
 apply_anyterm() {
@@ -59,7 +119,11 @@ apply_anyterm() {
   cp "$SCRIPT_DIR/terminal/sequences.txt" "$STATE_DIR"/user/generated/terminal/sequences.txt
   # Apply colors
   for i in "${!colorlist[@]}"; do
+    local clean_name="${colorlist[$i]#\$}"
+    local camel_name=$(echo "$clean_name" | sed -E 's/_([a-z])/\U\1/g')
     sed -i "s/${colorlist[$i]} #/${colorvalues[$i]#\#}/g" "$STATE_DIR"/user/generated/terminal/sequences.txt
+    sed -i "s/#${clean_name}#/${colorvalues[$i]}/g" "$STATE_DIR"/user/generated/terminal/sequences.txt
+    sed -i "s/#${camel_name}#/${colorvalues[$i]}/g" "$STATE_DIR"/user/generated/terminal/sequences.txt
   done
 
   sed -i "s/\$alpha/$term_alpha/g" "$STATE_DIR/user/generated/terminal/sequences.txt"
@@ -76,6 +140,9 @@ apply_anyterm() {
 apply_term() {
   apply_anyterm &
   apply_kitty &
+  apply_alacritty &
+  apply_ghostty &
+  apply_starship &
 }
 
 # Check if terminal theming is enabled in config
